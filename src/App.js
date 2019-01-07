557781d7e5e700853bbf68a0bca3f8620ac80afb
ryanpageKurtosys <ryan.page@kurtosys.com>
2019-01-07 12:13:15 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -6,41 +6,51 @@ import * as d3 from "d3";
 import { select } from 'd3-selection'
 
 
-const data = [
-  {width: 200, height: 100, fill: 'purple'},
-  {width: 100, height: 60, fill: 'pink'},
-  {width: 50, height: 30, fill: 'red'}
-];
+const dims = { height: 300, width: 300, radius: 150 };
+const cent = { x: (dims.width / 2 + 5), y: (dims.height / 2 + 5)};
+
 
 class App extends Component {
+
 
   componentDidMount(){
 
     const node = this.node;
 
-    const svg = d3.select(node);
+    const svg = d3.select(node)
+    .attr('width', dims.width + 150)
+    .attr('height', dims.height + 150);
 
-    const rects = svg.selectAll('rect')
-    .data(data)
+    const graph = d3.select(node)
+      .append('g')
+      .attr("transform", `translate(${cent.x}, ${cent.y})`);
 
-    rects.attr('width', d => d.width)
-  .attr('height', d => d.height)
-  .attr('fill', d => d.fill);
+    const pie = d3.pie()
+      .sort(null)
+      .value(d => d.cost);
 
-  const added = rects.enter()
-  .append('rect')
-    .attr('width', d => d.width)
-    .attr('height', d => d.height)
-    .attr('fill', d => d.fill);
+      const angles = pie([
+        { name: 'rent', cost: 500 },
+        { name: 'bills', cost: 300 },
+        { name: 'gaming', cost: 200 }
+      ]);
+      
+      const arcPath = d3.arc()
+        .outerRadius(dims.radius)
+        .innerRadius(dims.radius / 2);
+      
+      console.log(arcPath(angles[0]));
 
+      //https://naiksoftware.github.io/svg.html
+      
   }
 
   render() {
     return (
       <div className="App">
-        <header class="pink darken-1 section">
-            <h2 class="center white-text">D3 Example 2</h2>
-            <p class="flow-text grey-text center text-lighten-2"></p>
+        <header className="green darken-1 section">
+            <h2 className="center white-text">D3 Example 5</h2>
+            <p className="flow-text grey-text center text-lighten-2"></p>
        </header>
        <svg ref={node => this.node = node}/>
       </div>
