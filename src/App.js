75e6ee92b3a461c0c1fbdc09eb7b1098083ebf1a
ryanpageKurtosys <ryan.page@kurtosys.com>
2019-01-16 12:57:45 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header class="indigo darken-1 section">
-            <h2 class="center white-text">D3 Example 1</h2>
-            <p class="flow-text grey-text center text-lighten-2"></p>
+        <header className="indigo darken-1 section">
+            <h2 className="center white-text">D3 Example 1</h2>
+            <p className="flow-text grey-text center text-lighten-2"></p>
        </header>
        <div className="row">
         <div className="col m4 offset-m4">  
         <svg width="600" height="600">
           <rect x="300" y="100" fill="blue" width="100" height="200"></rect>
-          <circle cx='200' cy='200' r='50' fill='pink' stroke="red" stroke-width="2"></circle>
-          <line x1="100" y1="100" x2="120" y2="300" stroke="grey" stroke-width="3"></line>
+          <circle cx='200' cy='200' r='50' fill='pink' stroke="red"></circle>
+          <line x1="100" y1="100" x2="120" y2="300" stroke="grey"></line>
         </svg>
       </div>
       </div>
