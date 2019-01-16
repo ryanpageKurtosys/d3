a202baf2253a768e79c46cd0158b1e0534408500
ryanpageKurtosys <ryan.page@kurtosys.com>
2019-01-16 12:49:19 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ class App extends Component {
             <h2 class="center white-text">D3 Example 1</h2>
             <p class="flow-text grey-text center text-lighten-2"></p>
        </header>
+       <div className="row">
+        <div className="col m4 offset-m4">  
         <svg width="600" height="600">
           <rect x="300" y="100" fill="blue" width="100" height="200"></rect>
           <circle cx='200' cy='200' r='50' fill='pink' stroke="red" stroke-width="2"></circle>
           <line x1="100" y1="100" x2="120" y2="300" stroke="grey" stroke-width="3"></line>
         </svg>
+      </div>
+      </div>
       </div>
     );
   }
